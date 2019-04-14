18ccdde548586c524bdec20d3974f0b40d537d57
Dan Kaiser <Tophatman100@gmail.com>
2019-04-13 21:17:08 -0500

    add simple express hello world

--- src/index.js
+++ src/index.js
@@ -2,6 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import HelloWorld from './components/helloWorld';
 
-
-
 ReactDOM.render(<HelloWorld />, document.getElementById('app'));
