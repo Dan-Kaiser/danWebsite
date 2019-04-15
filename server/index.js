17a603f00372b85b8a4bbd0990f5eced4eb82145
Dan Kaiser <Tophatman100@gmail.com>
2019-04-15 14:10:49 -0500

    add simple static page to server

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.port || 3000;
 
-app.get('/', (req, res) => res.send('React Hello World!'))
+app.use(express.static('dist'))
+
+app.get('/test', (req, res) => res.send('Test Express Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
