ec32e442cb423651209c7bac7584f30b8ff5f7cf
Dan Kaiser <Tophatman100@gmail.com>
2019-04-21 00:12:26 -0500

    add placement functionality to tictactoe game

--- src/components/ticTacToe.js
+++ src/components/ticTacToe.js
@@ -4,18 +4,62 @@ class TicTacToe extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      currentTurn: 'X',
       boardState: [
-        0, 0, 0,
-        0, 0, 0,
-        0, 0, 0
+        '', '', '',
+        '', '', '',
+        '', '', ''
       ]
     };
+    this.setState = this.setState.bind(this);
+    this.clickHandler = this.clickHandler.bind(this);
+    this.changeTurn = this.changeTurn.bind(this);
+  }
+
+  clickHandler(event) {
+    const { id, textContent } = event.target;
+    this.updateTile(textContent, id);
+  }
+
+  updateTile(val, index) {
+    console.log(val, index);
+    if (!val) {
+      let { boardState, currentTurn } = this.state;
+      boardState[index] = currentTurn;
+      this.setState({ boardState });
+      this.changeTurn();
+    }
+  }
+
+  changeTurn() {
+    if (this.state.currentTurn === 'X') {
+      this.setState({ currentTurn: 'O' })
+    } else {
+      this.setState({ currentTurn: 'X' })
+    }
   }
 
   render() {
     return (
-      <div id="tTCBoard">
-        <div id='ticTacToe' className='ticTacToe'>
+      <div>
+        <div>
+          currentTurn : {this.state.currentTurn}
+        </div>
+
+        <div id="tTCBoard">
+          <div id='ticTacToe' className='ticTacToe'>
+            {this.state.boardState.map((value, index) => {
+              return (
+                <div
+                  className='tile'
+                  id={index}
+                  key={index}
+                  onClick={this.clickHandler}>
+                  {value}
+                </div>
+              )
+            })}
+            {/* <div className='tile'>0</div>
           <div className='tile'>0</div>
           <div className='tile'>0</div>
           <div className='tile'>0</div>
@@ -23,8 +67,8 @@ class TicTacToe extends React.Component {
           <div className='tile'>0</div>
           <div className='tile'>0</div>
           <div className='tile'>0</div>
-          <div className='tile'>0</div>
-          <div className='tile'>0</div>
+          <div className='tile'>0</div> */}
+          </div>
         </div>
       </div>
     )
